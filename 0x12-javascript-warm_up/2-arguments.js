29149fa1273505cf1e5ec9d88e1155994968b9d5
AdamsGeeky <muhdadamu0008@gmail.com>
2023-06-01 16:15:04 +0100

    freedom or die the only way

--- 0x12-javascript-warm_up/2-arguments.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-if (process.argv.length <= 2) {
-  console.log('No argument');
-} else {
-  console.log((process.argv.length > 3 ? 'Arguments' : 'Argument') + ' found');
-}
--- 0x12-javascript-warm_up/README.md
+++
@@ -1,1 +0,0 @@
-my readme on javascript warm up
