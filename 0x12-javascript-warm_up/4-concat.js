bb3780244ec02750777731ad8f29e337fd0bae4a
ADAMU MUHAMMAD MUHAMMAD <adamsgeeky@localhost.localdomain>
2023-06-13 18:21:41 +0100

    js tasks: 1,2,3,4,5,

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
 const args = process.argv;
-if(args[2] === undefined && args[3] === undefined){
-    console.log('undefined is undefined');
-}
-else if(args[2] === undefined){
-    console.log('undefined is ' + args[3]);
-}
-else if(args[3] === undefined){
-    console.log(args[2] + ' is undefined');
-}
-else{
-    console.log(args[2] + ' is ' + args[3]);
+if (args[2] === undefined && args[3] === undefined) {
+  console.log('undefined is undefined');
+} else if (args[2] === undefined) {
+  console.log('undefined is ' + args[3]);
+} else if (args[3] === undefined) {
+  console.log(args[2] + ' is undefined');
+} else {
+  console.log(args[2] + ' is ' + args[3]);
 }
